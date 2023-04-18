55187147807ada258f91866ce42bb6b830ace49e
fgavica <104703256+fgavica@users.noreply.github.com>
2023-04-18 23:31:01 +0200

    firebase

--- app/src/main/java/tm/wearable/wearabletfm/MainActivity.kt
+++ app/src/main/java/tm/wearable/wearabletfm/MainActivity.kt
@@ -1,10 +1,16 @@
 package tm.wearable.wearabletfm
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import tm.wearable.wearabletfm.databinding.ActivityMainBinding
 import tm.wearable.wearabletfm.ui.LoginActivity
+
+import android.Manifest
+import android.content.pm.PackageManager
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -16,6 +22,38 @@ class MainActivity : AppCompatActivity() {
 
         binding.cvContinue.setOnClickListener {
             startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+        }
+        askNotificationPermission()
+
+
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // FCM SDK (and your app) can post notifications.
+        } else {
+            // TODO: Inform user that that your app will not show notifications.
+        }
+    }
+
+    private fun askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                // FCM SDK (and your app) can post notifications.
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                // TODO: display an educational UI explaining to the user the features that will be enabled
+                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                //       If the user selects "No thanks," allow the user to continue without notifications.
+            } else {
+                // Directly ask for the permission
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
         }
     }
 }
---
+++ app/src/main/java/tm/wearable/wearabletfm/MyFirebaseMessagingService.kt
@@ -0,0 +1,4 @@
+package tm.wearable.wearabletfm
+
+class MyFirebaseMessagingService {
+}
